470908ee137f17a651ffd3880003dc06b791d2c6
leticia.nascimento@sptech.school <letscostz>
2025-11-26 16:06:23 -0300

    Testando a Captura Id

--- web-teste-mundo/src/controllers/resultadoController.js
+++ web-teste-mundo/src/controllers/resultadoController.js
@@ -31,7 +31,29 @@ function inserirResultado(req, res) {
 
 function capturarId(req, res) {
 
-  var resultado = 
+  var resultado = req.body.idResultado;
+  console.log(resultado);
+
+  if (resultado == undefined) {
+        res.status(400).send("resultado está undefined!");
+    } else {
+
+        resultadoModel.capturarId()
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao Capturar Id! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
 }
 
 function exibirResultado(req, res) {
@@ -58,5 +80,6 @@ function exibirResultado(req, res) {
 
 module.exports = {
     inserirResultado,
-    exibirResultado
+    exibirResultado,
+    capturarId
 }
