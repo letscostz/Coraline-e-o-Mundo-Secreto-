11b4f8c4846c0682fd69ce7a8ab35010ee027b80
letscostz <leticia.nascimento@sptech.school>
2025-12-03 05:29:27 -0300

    organizando as pastas e arquivos

--- web-teste-mundo/app.js
+++ web-teste-mundo/app.js
@@ -17,10 +17,6 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
 var resultadosRouter = require("./src/routes/resultado");
 
 app.use(express.json());
@@ -31,10 +27,6 @@ app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
 app.use("/resultados", resultadosRouter);
 
 app.listen(PORTA_APP, function () {
